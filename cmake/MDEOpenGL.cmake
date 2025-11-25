44f0e720bc4993bcbb43fc7e05eee750a00727ec
NishiOwO <nishi@nishi.boats>
2025-11-26 07:13:29 +0900

    opengl

---
+++ cmake/MDEOpenGL.cmake
@@ -0,0 +1,26 @@
+include_guard(DIRECTORY)
+include(MDEPkgConfig)
+
+macro(opengl_add name)
+	if(CMAE_SYSTEM_NAME STREQUAL "Windows")
+		target_link_libraries(
+			${name}
+			PRIVATE
+			opengl32
+		)
+	else()
+		pkg_config_add(${name} gl)
+	endif()
+endmacro()
+
+macro(glu_add name)
+	if(CMAE_SYSTEM_NAME STREQUAL "Windows")
+		target_link_libraries(
+			${name}
+			PRIVATE
+			glu32
+		)
+	else()
+		pkg_config_add(${name} glu)
+	endif()
+endmacro()
